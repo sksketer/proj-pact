c62264bb85cbb84d92eeedbc84e350fb628883bd
sketer <ksamrit121@gmail.com>
2024-09-02 23:31:35 +0530

    add IGame interface and add game ref in window global object, also init pixi application

---
+++ src/interface/Interface.ts
@@ -0,0 +1,7 @@
+import { Application } from "pixi.js";
+import { Constants } from "../ts/constant/Constants";
+
+export interface IGame {
+    currentGame?: Application;
+    constants?: typeof Constants;
+}
--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,9 +1,23 @@
 import { Application, Assets, Sprite } from 'pixi.js';
+import { Constants } from './constant/Constants';
+import { IGame } from '../interface/Interface';
 import '../styles/style.less';
-import { Constants } from './constant/constants';
 import updateLoader from './loader/loader';
 
 const msg: string = 'I Like you, shruti!';
 console.log(msg);
 
-setTimeout(updateLoader, Constants.hideLoaderDelay);
+/** ----------------------------------- */
+
+// store Game reference in Global game object
+let Game: IGame = {};
+(window as any).game = Game;
+
+const app = new Application();
+await app.init().then((res: any) => {
+    console.error('promis resolve: ', res);
+    document.body.appendChild(app.canvas);
+});
+
+Game.currentGame = app;
+Game.constants = Constants;
